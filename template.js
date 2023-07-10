7fd647ac47568fa9724244baa7fa3a1372cee14a
shivbanga <138822006+shivbanga@users.noreply.github.com>
2023-07-10 12:53:04 -0500

    Add files via upload

---
+++ template.js
@@ -0,0 +1,20 @@
+//template
+
+let 
+
+function preload()
+{
+	furby = loadImage('furby.png')
+}//preload
+
+function setup()
+{
+	createCanvas(windowWidth, windowHeight)
+	
+}//setup
+
+function draw()
+{
+
+}//draw
+
