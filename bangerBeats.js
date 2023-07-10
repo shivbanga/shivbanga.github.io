e514d1f79f308fb9f40b3fd43f8b3a2a63dd2c12
shivbanga <138822006+shivbanga@users.noreply.github.com>
2023-07-10 14:01:10 -0500

    Add files via upload

--- bangerBeats.js
+++ bangerBeats.js
@@ -60,9 +60,9 @@ function homeScreen()
 	background(200, 30, 100);
 	fill(255);
 	textSize(40);
-	text("Welcome to Banga Beats", windowWidth/3, 50)
+	text("Welcome to Banga Beats", 500, 50)
 	textSize(20)
-	text("press mouse pad to continue", windowWidth/2.5,100)
+	text("press mouse pad to continue", 600,100)
 }
 
 function start()
